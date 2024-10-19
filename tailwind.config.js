2a7afff89ed6a9bb4a9b9ce9190d0fbfce85f1ca
Yannic Fréson <61149527+yannicfreson@users.noreply.github.com>
2024-10-20 00:54:48 +0200

    Start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                aring: ['"Aring"', "sans-serif"],
+                balbek: ['"Balbek"', "sans-serif"],
+                wanderer: ['"Wanderer"', "sans-serif"],
+                asteroid: ['"Asteroid"', "sans-serif"],
+                ringstun: ['"Ringstun"', "sans-serif"],
+            },
+            colors: {
+                "accent-light": "#eea20c",
+                "accent-dark": "#f0980e",
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
