efa20e1389d8f18292a5d3b517e95abbd4b8fa71
Yannic Fréson <61149527+yannicfreson@users.noreply.github.com>
2024-10-20 22:19:58 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,43 +1,50 @@
 export default function Home() {
-    let name = "Fréson"
-    let email = "contact@yannicfreson.be"
-    let website = "www.yannicfreson.be"
-    let phone = "+32 123 45 67 89"
-    let tax = "BE 0804 626 876"
-    let services = ["Nightlife", "Events", "Potraits"]
+    const userInfo = {
+        name: "Fsëvss",
+        email: "cppwehzGjxyuptuBw{",
+        website: "wxy1}ftuqlp}q}}>sw",
+        phone: "+44#8<6'>?*>>-DD",
+        tax: "BF\"3<5:'>;@+DDD",
+        qrCodeUrl: "huvsw?569n|}ytxyF{",
+    }
+
+    function decode(data) {
+        return data
+            .split("")
+            .map((c, i) => {
+                return String.fromCharCode(c.charCodeAt(0) - i)
+            })
+            .join("")
+    }
 
     return (
         <div className="flex flex-col items-center justify-center bg-black h-svh w-svw text-black p-8">
             <div
-                className="bg-neutral-100 w-full aspect-[1/1.75] rounded-3xl flex flex-col justify-center items-center gap-8
+                className="bg-neutral-100 w-full aspect-[1/1.75] rounded-3xl flex flex-col justify-center items-center gap-6 p-8 border-accent-light border-2
             "
             >
-                <img src="/profile_picture.jpg" alt="Yannic Fréson" className="rounded-full aspect-square object-cover object-top w-1/2" />
-                <div className="flex flex-col items-center">
-                    <p className="">{email}</p>
-                    <p className="">{website}</p>
-                    <p className="">{phone}</p>
-                </div>
-
-                <div className="flex flex-row gap-4 font-sans justify-center items-center">
-                    {services.map((service) => (
-                        <p key={service} className="">
-                            {service}
-                        </p>
-                    ))}
-                </div>
-
-                <div className="font-asteroid -ml-14 flex text-8xl text-accent-light">
+                <div className="font-asteroid -ml-10 flex text-6xl text-accent-light -mb-2">
                     {
                         // first letter of the name
-                        <p className="scale-y-50">{name.charAt(0)}</p>
+                        <p className="scale-y-50">{decode(userInfo.name).charAt(0)}</p>
                     }
                     {
                         // the rest of the name
-                        <p className="scale-y-75 mt-5 -ml-4">{name.slice(1)}</p>
+                        <p className="scale-y-75 mt-3 -ml-3">{decode(userInfo.name).slice(1)}</p>
                     }
                 </div>
-                <p className="">{tax}</p>
+
+                <img src="/profile_picture.jpg" alt="Yannic Fréson" className="rounded-full aspect-square object-cover object-top w-1/2 border-2 border-accent-light" />
+
+                <div className="flex flex-col font-bold items-center">
+                    <p className="">{decode(userInfo.phone)}</p>
+                    <p className="">{decode(userInfo.email)}</p>
+                    <p className="">{decode(userInfo.website)}</p>
+                </div>
+
+                <img src={qrCodeUrl ? `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${decode(userInfo.qrCodeUrl)}` : "https://via.placeholder.com/150"} alt="QR code" className="w-1/2" />
+
+                <p className="font-bold">{decode(userInfo.tax)}</p>
             </div>
         </div>
     )
